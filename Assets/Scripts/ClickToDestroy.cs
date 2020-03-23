5335ecbfbd2401a307e9c82beb14f263faaebf8b
Rocco <roccoestrada@gmail.com>
2020-03-22 22:27:58 -0700

    first commit

---
+++ Assets/Scripts/ClickToDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToDestroy : MonoBehaviour
+{
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit))
+            {
+
+                BoxCollider bc = hit.collider as BoxCollider;
+                if(bc.tag == "Enemy")
+                {
+                    Destroy(bc.gameObject);
+                }
+            }
+        }
+    }
+}
